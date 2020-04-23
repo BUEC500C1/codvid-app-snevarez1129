dc69cc20ea605c384d0402b0263987228b494baf
Sebastian Nevarez <snevarez@bu.edu>
2020-04-23 12:59:32 -0400

    edits to map view

--- covid/App.js
+++ covid/App.js
@@ -5,8 +5,7 @@ import MapView, { } from 'react-native-maps';
 const styles = StyleSheet.create({
  container: {
    ...StyleSheet.absoluteFillObject,
-   height: 400,
-   width: 400,
+   flex: 1,
    justifyContent: 'flex-end',
    alignItems: 'center',
  },
@@ -20,8 +19,8 @@ export default () => (
      <MapView
        style={styles.map}
        region={{
-         latitude: 37.78825,
-         longitude: -122.4324,
+         latitude: 42.3601,
+         longitude: -71.0589,
          latitudeDelta: 0.015,
          longitudeDelta: 0.0121,
        }}
